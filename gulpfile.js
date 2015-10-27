ada4edcfc6ac43a6f87b2d912dbe4808b673317a
Lindsay Pichie <lpichie1@gmail.com>
2015-10-27 16:36:21 -0400

    Lindsays updates - very mminor

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp   = require('gulp'),
 		sass = require('gulp-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
-		concat = require('gulp-concat');
+		concat = require('gulp-concat'),
+		browserSync = require('browser-sync'),
+		reload = browserSync.reload;
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('bs', function(){
+	browserSync.init({
+		proxy: 'http://localhost:8888/week7project/'
+	});
+});
+	
+gulp.task('default', ['bs', 'styles', 'watch']);
 
 gulp.task('styles', function() {
 	return gulp.src('sass/**/*.scss')
@@ -12,10 +20,13 @@ gulp.task('styles', function() {
 			errLogToConsole: true
 		}))
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('.'));
+		.pipe(gulp.dest('.'))
+		.pipe(reload({ stream: true }));
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
 	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('**/*.php', reload);
+
 });
