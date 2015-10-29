6812e0c93eafc3eb44c63d0142a1db3d34743359
Giordan Battaglin <gio@giordan.ca>
2015-10-29 16:03:58 -0400

    featured custom type now working

--- splash.php
+++ splash.php
@@ -16,7 +16,7 @@ include('headerSplash.php');  ?>
 		<button class="mainBtn">Work With Us</button>
 
 
-  </div> <!-- /.container -->
+  </div> /.container
 </div> <!-- /.main -->
 
 <section class="body">
@@ -40,11 +40,39 @@ include('headerSplash.php');  ?>
 
 <section class="feature">
 	<h4>Don't wait! Get started on your path to wellness today.</h4>
-	<h2>Featured Plans</h2>
+	
+	<!-- shows 3 featured plans -->
+
+	<?php 
+
+		$featureQuery = new WP_Query(
+				array(
+						'posts_per_page' => 3,
+						'post_type' => 'feature',
+						'post_not_in' => array( $post->ID )
+					)
+			);
+
+	 ?>
+
+	 <?php 
+
+	 	if ($featureQuery->have_posts()) : 
+
+	  ?>
+
+			<?php while ($featureQuery->have_posts()) : $featureQuery->the_post(); ?>
+	
+			<h2><?php the_title() ?></h2>
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+	
 </section>
 
 <section class="book">
 	
+
 </section>
 
 <section class="latest">
